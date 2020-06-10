f2e730f71d87623e1058b28e7c572bc007a33a78
Elazar Fein <elazar170@gmail.com>
2020-06-10 12:47:58 +0300

    Added database

--- server.js
+++ server.js
@@ -21,13 +21,21 @@ let ips =[];
 let count = 0;
 let nindex = 2;
 let countIp = 0;
+require("dotenv").config();
 var admin = require("firebase-admin");
-
+console.log(process.env.FIREBASE_PROJECT_ID);
 admin.initializeApp({
   credential: admin.credential.cert({
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "type": process.env.FIREBASE_TYPE,
     "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
   }),
   databaseURL: "https://chatapp-skysibe.firebaseio.com"
 });
